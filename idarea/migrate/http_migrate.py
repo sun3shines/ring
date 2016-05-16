ae53d6aef64e62eab980165a00d57edb550fc351
root <sun__shines@163.com>
2016-05-16 14:59:12 +0800

    ss

--- idarea/migrate/http_migrate.py
+++ idarea/migrate/http_migrate.py
@@ -2,7 +2,11 @@
 
 import json
 from idarea.common.http import jresponse
+from idarea.object.md5st import MSt
+from idarea.common.webobx import Request, Response
 from idarea.common.libpart import merge_part
+from idarea.migrate.static import migrateObj
+from idarea.ring.variable import CURRENT_RING_SEQ
 
 def doPartTransmit(request):
     
@@ -11,6 +15,11 @@ def doPartTransmit(request):
     seq = int(param.get('seq'))
     md5_list = param.get('md5list')
     merge_part(part, seq, md5_list)
+    
+    if seq < CURRENT_RING_SEQ:
+        migrateObj.PAST_QUEUE.put((int(part),seq)) 
+    else:
+        migrateObj.LATEST_QUEUE.put(int(part))
         
     return jresponse('0','',request,200)
 
